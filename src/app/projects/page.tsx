6d09b27c7eb5c82658f3c962311f78a4c797b7b6
mehdi vaezi <mh.vaezi@gmail.com>
2025-05-26 09:37:33 +0330

    :sparkles: add kamandar

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,5 @@
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3dCard";
+import Image from "next/image";
 
 export default function ProjectsPage() {
   return (
@@ -13,8 +14,8 @@ export default function ProjectsPage() {
             Hover over this card to unleash the power of CSS perspective
           </CardItem>
           <CardItem translateZ="100" className="mt-4 w-full">
-            <img
-              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            <Image
+              src="/images/kamandar.jpg"
               height="1000"
               width="1000"
               className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
